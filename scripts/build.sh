da92d877e0215f7358dc8565a512fa0aedbf9fea
Casey Riebe <casey@riebe.cloud>
2026-01-17 15:00:38 -0600

    Switching from guestfish to qemu-nbd and chroot

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,22 +8,34 @@ if [ "${CHROOT:-'unset'}" == "unset" ]; then
 fi
 version="${VERSION:-dev}"
 
-if [ -z "$(which guestfish)" ]; then
-    sudo apt-get install -y guestfish
+if [ -z "$(which qemu-nbd)" ]; then
+    sudo apt-get install -y qemu-utils
 fi
+
+rm -f *.qcow2
+rm -f *.qcow2.sha256
 
 set -o xtrace
 
-sudo guestfish --rw -i -x --network -a "$CHROOT/scripts/debian-13-generic-amd64.qcow2" <<_EOF_
-command "apt-get update"
-command "apt-get install -y qemu-guest-agent"
-command "systemctl enable qemu-guest-agent"
-command "truncate -s 0 /etc/machine-id"
-command "rm -f /var/lib/dbus/machine-id"
-command "cloud-init clean --logs"
-command "apt-get clean"
-command "rm -rf /var/log/*"
-command "rm -rf /var/lib/apt/lists/*"
-_EOF_
+sudo modprobe nbd max_part=8
+sudo qemu-nbd --connect=/dev/nbd0 "$CHROOT/scripts/debian-13-generic-amd64.qcow2"
+mkdir -p "$CHROOT/mnt"
+sleep 1  # Gives the system a beat to ensure the nbd mounts exist
+sudo mount /dev/nbd0p1 "$CHROOT/mnt"
+sudo mount --bind /dev "$CHROOT/mnt/dev"
+sudo mount --bind /proc "$CHROOT/mnt/proc"
+sudo mount --bind /sys "$CHROOT/mnt/sys"
+sudo mount --bind /run "$CHROOT/mnt/run"
+
+sudo chroot "$CHROOT/mnt" apt-get update
+sudo chroot "$CHROOT/mnt" apt-get install -y qemu-guest-agent
+
+sudo umount "$CHROOT/mnt/dev"
+sudo umount "$CHROOT/mnt/proc"
+sudo umount "$CHROOT/mnt/sys"
+sudo umount "$CHROOT/mnt/run"
+sudo umount -l "$CHROOT/mnt"
+sudo qemu-nbd --disconnect /dev/nbd0
+
 mv "$CHROOT/scripts/debian-13-generic-amd64.qcow2" "orbitlab-debian-13-amd64-${version}.qcow2"
 sha256sum "orbitlab-debian-13-amd64-${version}.qcow2" > "orbitlab-debian-13-amd64-${version}.qcow2.sha256"
